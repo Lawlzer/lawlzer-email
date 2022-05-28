e316e65dcf7387db9b475cb1aa9137dbc4d9f82c
kevindaspam@gmail.com <kevindaspam@gmail.com>
2022-05-27 22:32:29 -0500

    removed "Keys" exported function

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,6 @@ export type emailConfig = {
     presetDirectory: string;
 };
 
-export function keys<O extends object>(obj: O): Array<keyof O> {
-    return Object.keys(obj) as Array<keyof O>;
-}
 
 export async function config(config: emailConfig): Promise<void> {
     if (!config.sendgridAPIKey) throw new Error('@lawlzer/email - config - sendgridAPIKey is required');
